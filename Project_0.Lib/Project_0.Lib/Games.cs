761e3a6102e5c9bbb1a701d2e135f7e2e78f582c
Impstar <magesbe@gmail.com>
2019-02-25 13:59:03 -0600

    beginning to implement project 0

---
+++ Project_0.Lib/Project_0.Lib/Games.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Project_0.Lib
+{
+    public abstract class Games
+    {
+
+        protected virtual string Name { get; set; } //Name of game
+        protected virtual double Cost { get; set; } //Base cost of game
+        protected virtual double AdvancedCost { get; set; } //Advanced cost of game
+        protected virtual double DeluxeCost { get; set; } //Delux cost of game
+
+    }
+}
---
+++ Project_0.Lib/Project_0.Lib/IsekaiQuest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_0.Lib
+{
+    public class IsekaiQuest : Games
+    {
+        //Contains fields declared in Games
+    }
+}
